af3bcbc19283dcf03bd1ed8789d9cc614abcdd99
Jacob Colapietro <jacob.colapietro@vector.com>
2026-06-16 13:23:35 -0400

    Starting batch script for getting the commands and then creating a new database

---
+++ vcdb_add_paths.bat
@@ -0,0 +1,11 @@
+echo "Creating minimal CCAST_.CFG (needed for vcdb)"
+echo "C_COMPILE_CMD: tricore-c++" > CCAST_.CFG
+
+echo "Running vcdb get commands"
+%VECTORCAST_DIR%\vcdb --db=vcshell.db getallcmdlines --all > commands.txt
+
+echo "Add include paths for source files that are not included already"
+
+
+echo "Importing filtered commands into new database"
+%VECTORCAST_DIR%\vcshell --db=vcshell_new.db --inputcmds=commands_filtered.txt putcommand
